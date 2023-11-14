34f28274b47153c2d1ecb81bb70288a730840c2c
Elijah Durston-Rand <elijah@elijahdr.com>
2023-11-14 15:18:19 +0000

    Parallel workers working, 2nd test

--- gol/distributor.go
+++ gol/distributor.go
@@ -15,6 +15,48 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+func makeImmutableMatrix(matrix [][]uint8) func(y, x int) uint8 {
+	return func(y, x int) uint8 {
+		return matrix[y][x]
+	}
+}
+
+// type insert[T any] func (array[]T, item T, i int) []T {
+
+// }
+
+func insert(array [][]byte, item []byte, i int) [][]byte {
+	newArray := append(array, []byte{0})
+	copy(newArray[i+1:], newArray[i:])
+	newArray[i] = item
+	return newArray
+}
+
+func calcSteps(p Params) []int {
+	var steps []int
+	for i := 0; i < p.Threads; i++ {
+		steps = append(steps, 0)
+	}
+	c := 0
+	for i := 0; i < p.ImageHeight; i++ {
+		steps[c%p.Threads]++
+		c++
+	}
+
+	return steps
+}
+
+func calcStartY(p Params) []int {
+	steps := calcSteps(p)
+	var startY []int
+	startY = append(startY, 0)
+	for i := 1; i <= p.Threads; i++ {
+		startY = append(startY, startY[i-1]+steps[i-1])
+	}
+
+	return startY
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
@@ -44,8 +86,44 @@ func distributor(p Params, c distributorChannels) {
 	}
 
 	// TODO: Execute all turns of the Game of Life.
+
+	// for i := 0; i < p.Turns; i++ {
+	// 	world = calculateStep(p, world)
+	// }
+
+	startX := 0
+	endX := p.ImageWidth
+
+	startY := calcStartY(p)
+
 	for i := 0; i < p.Turns; i++ {
-		world = calculateStep(p, world)
+		immutableWorld := makeImmutableMatrix(world)
+
+		channels := make([]chan [][]byte, p.Threads)
+		for i := 0; i < len(channels); i++ {
+			channels[i] = make(chan [][]byte)
+
+			// step := int(math.Ceil(float64(p.ImageHeight / p.Threads)))
+			// startY := (i * step)
+			// endY := int(math.Min(float64(startY+step), float64(p.ImageHeight)))
+			// fmt.Println(step, startY)
+
+			go worker(channels[i], p, immutableWorld, startY[i], startY[i+1], startX, endX)
+		}
+
+		var newWorld [][]byte
+		for _, channel := range channels {
+			data := <-channel
+			for _, d := range data {
+				newWorld = append(newWorld, d)
+			}
+		}
+
+		world = newWorld
+
+		c.events <- TurnComplete{
+			CompletedTurns: i,
+		}
 	}
 
 	// fmt.Println("All done")
@@ -67,6 +145,20 @@ func distributor(p Params, c distributorChannels) {
 	close(c.events)
 }
 
+func worker(channel chan [][]byte, p Params, world func(y, x int) uint8, startY int, endY int, startX int, endX int) {
+	var newWorld [][]byte
+	for y := startY; y < endY; y++ {
+		var newLine []byte
+		for x := startX; x < endX; x++ {
+			n := neighbours(p, world, []int{x, y})
+			newLine = append(newLine, golLogic(world(y, x), n))
+		}
+		newWorld = append(newWorld, newLine)
+	}
+
+	channel <- newWorld
+}
+
 func calcAliveCells(world [][]uint8) []util.Cell {
 	cells := make([]util.Cell, 0)
 	for y, line := range world {
@@ -82,7 +174,7 @@ func calcAliveCells(world [][]uint8) []util.Cell {
 	return cells
 }
 
-func neighbours(world [][]byte, pos []int) int {
+func neighbours(p Params, world func(y, x int) uint8, pos []int) int {
 	count := 0
 	for dy := -1; dy <= 1; dy++ {
 		for dx := -1; dx <= 1; dx++ {
@@ -91,14 +183,33 @@ func neighbours(world [][]byte, pos []int) int {
 			}
 			//fmt.Println(x+dx, y+dy)
 			//fmt.Println(world)
-			newPos := [2]int{(pos[0] + dx + len(world[0])) % len(world[0]), (pos[1] + dy + len(world)) % len(world)}
-			if world[newPos[0]][newPos[1]] == 255 {
+			// Add the len of the line/world so that if it is negative, return a positive result.
+			newPos := [2]int{(pos[0] + dx + p.ImageWidth) % p.ImageWidth, (pos[1] + dy + p.ImageHeight) % p.ImageHeight}
+			if world(newPos[1], newPos[0]) == 255 {
 				count += 1
 			}
 		}
 	}
 
 	return count
+}
+
+func golLogic(current byte, nCount int) byte {
+	if current == 255 {
+		if nCount < 2 {
+			return 0
+		} else if nCount > 3 {
+			return 0
+		} else {
+			return 255
+		}
+	} else {
+		if nCount == 3 {
+			return 255
+		} else {
+			return 0
+		}
+	}
 }
 
 func calculateStep(p Params, world [][]byte) [][]byte {
